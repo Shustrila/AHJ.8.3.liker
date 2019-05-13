fde6563423a0683a1660e7cd4db5d92fa844eb4e
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-05-13 15:35:55 +0300

    fis eslint

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import './css/style.css';
-import './js/main.js';
+import './js/main';
--- src/js/Like.js
+++ src/js/Like.js
@@ -1,43 +1,42 @@
 import heartImg from '../images/heart.png';
 
 class Like {
-    constructor(button) {
-        this.button = button;
-        this.srcImg = heartImg;
-        this.arr = ['--as', '--as-reverse', '--ac', '--ac-reverse'];
+  constructor(button) {
+    this.button = button;
+    this.srcImg = heartImg;
+    this.arr = ['--as', '--as-reverse', '--ac', '--ac-reverse'];
 
-        this.init();
-    }
+    this.init();
+  }
 
 
+  imageHeart() {
+    return new Promise((resolve) => {
+      const heart = document.createElement('img');
+      const random = Math.floor(Math.random() * this.arr.length);
 
-    imageHeart() {
-        return new Promise(resolve => {
-            const heart = document.createElement('img');
-            const random = Math.floor(Math.random() * this.arr.length);
+      heart.className = `like__heart like__heart${this.arr[random]}`;
+      heart.src = this.srcImg;
 
-            heart.className = 'like__heart like__heart' + this.arr[random];
-            heart.src = this.srcImg;
+      heart.addEventListener('animationend', e => e.target.remove());
 
-            heart.addEventListener('animationend', (e) => e.target.remove());
+      resolve(heart);
+    });
+  }
 
-            resolve(heart);
-        })
-    }
+  async onClickButton(e) {
+    e.preventDefault();
 
-     async onClickButton(e) {
-        e.preventDefault();
+    const heart = await this.imageHeart();
 
-        const heart = await this.imageHeart();
+    e.currentTarget.appendChild(heart);
+  }
 
-        e.currentTarget.appendChild(heart);
-    }
+  init() {
+    const button = document.querySelector(this.button);
 
-    init() {
-        const button = document.querySelector(this.button);
-
-        button.addEventListener('click', (e) => this.onClickButton(e))
-    }
+    button.addEventListener('click', e => this.onClickButton(e));
+  }
 }
 
 export default Like;
